907247df58c94211f6785cdd8e6ad6bc8ac8a622
JamesGood626 <jamesgood626@gmail.com>
2019-05-26 17:48:10 -0400

    All of auth_controller.ex refactored out to service functions.

--- lib/budget_app/auth/services/auth_service.ex
+++ lib/budget_app/auth/services/auth_service.ex
@@ -34,6 +34,58 @@ defmodule BudgetApp.AuthService do
     Map.put(session_data, :expiry, expiry)
   end
 
+  def signup_user(conn, %{"email" => email, "password" => password} = params) do
+    case CredentialServer.get_user(email) do
+      {:ok, _user} ->
+        # status code?
+        json(conn, %{message: "That email is taken."})
+
+      # Move all this logic into the Auth Module
+      {:err, _message} ->
+        hash = AuthService.hash_password(password)
+
+        Map.put(params, "password", hash)
+        |> Map.put_new("active", false)
+        |> CredentialServer.create_credentials()
+
+        short_token = AuthService.generate_short_token()
+        CredentialServer.add_short_token(email, short_token)
+        Email.send_signup_email(short_token, email)
+        json(conn, %{message: "you've successfully requested to sign up #{email}"})
+    end
+  end
+
+  def approve_sign_up(conn, %{"short_token" => short_token, "user_email" => user_email} = params) do
+    {:ok, user} = CredentialServer.get_user(user_email)
+
+    case user["short_token"] === short_token do
+      true ->
+        # Move all this to a separate function (/Auth Module) which uses a when block?
+        CredentialServer.remove_short_token(user_email)
+        CredentialServer.activate_user(user_email)
+        BudgetSupervisor.start_budget(user_email)
+        Email.send_notify_email(user_email)
+        json(conn, %{message: "you've successfully approved the sign up"})
+
+      false ->
+        json(conn, %{message: "Invalid short token!"})
+    end
+  end
+
+  def logout_user(conn) do
+    %{email: email} = get_session(conn, :session_token)
+
+    case CredentialServer.remove_hashed_remember_token(email) do
+      {:ok, _msg} ->
+        conn
+        |> put_session(:session_token, %{})
+        |> json(%{message: "Logout Success!"})
+
+      {:err, _msg} ->
+        json(conn, %{message: "Logout Failed!"})
+    end
+  end
+
   def login_user(conn, %{"email" => email, "password" => password} = params) do
     case CredentialServer.get_user(email) do
       {:ok, user} ->
--- lib/budget_app_web/controllers/auth_controller.ex
+++ lib/budget_app_web/controllers/auth_controller.ex
@@ -7,54 +7,18 @@ defmodule BudgetAppWeb.AuthController do
 
   def csrf(conn, _params) do
     csrf_token = get_csrf_token()
-    IO.puts("A CSRF TOKEN:")
-    IO.inspect(csrf_token)
     json(conn, %{csrf_token: csrf_token})
   end
 
   def signup(conn, %{"email" => email, "password" => password} = params) do
-    IO.puts("SIGNUP ROUTE HIT")
-    # All of these should really be delegated to some service functions.
-    case CredentialServer.get_user(email) do
-      {:ok, _user} ->
-        # status code?
-        json(conn, %{message: "That email is taken."})
-
-      # Move all this logic into the Auth Module
-      {:err, _message} ->
-        hash = AuthService.hash_password(password)
-
-        Map.put(params, "password", hash)
-        |> Map.put_new("active", false)
-        |> CredentialServer.create_credentials()
-
-        short_token = AuthService.generate_short_token()
-        CredentialServer.add_short_token(email, short_token)
-        Email.send_signup_email(short_token, email)
-        json(conn, %{message: "you've successfully requested to sign up #{email}"})
-    end
+    AuthService.signup_user(conn, params)
   end
 
-  def approve_sign_up(conn, %{"short_token" => short_token, "user_email" => user_email}) do
-    # All of these should really be delegated to some service functions.
-    {:ok, user} = CredentialServer.get_user(user_email)
-
-    case user["short_token"] === short_token do
-      true ->
-        # Move all this to a separate function (/Auth Module) which uses a when block?
-        CredentialServer.remove_short_token(user_email)
-        CredentialServer.activate_user(user_email)
-        BudgetSupervisor.start_budget(user_email)
-        Email.send_notify_email(user_email)
-        json(conn, %{message: "you've successfully approved the sign up"})
-
-      false ->
-        json(conn, %{message: "Invalid short token!"})
-    end
+  def approve_sign_up(conn, %{"short_token" => short_token, "user_email" => user_email} = params) do
+    AuthService.approve_sign_up(conn, params)
   end
 
   def deny_sign_up(conn, %{"short_token" => short_token, "user_email" => user_email}) do
-    # All of these should really be delegated to some service functions.
     CredentialServer.remove_user(user_email)
     json(conn, %{message: "you've successfully denied the sign up!"})
   end
@@ -64,16 +28,6 @@ defmodule BudgetAppWeb.AuthController do
   end
 
   def logout(conn, _params) do
-    %{email: email} = get_session(conn, :session_token)
-
-    case CredentialServer.remove_hashed_remember_token(email) do
-      {:ok, _msg} ->
-        conn
-        |> put_session(:session_token, %{})
-        |> json(%{message: "Logout Success!"})
-
-      {:err, _msg} ->
-        json(conn, %{message: "Logout Failed!"})
-    end
+    AuthService.logout_user(conn)
   end
 end
